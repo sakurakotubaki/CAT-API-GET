72763cf23be0524309c9186e1d518301ae3065b7
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2022-10-22 11:45:17 +0900

    [add]
    useStateを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,39 @@
+import { log } from 'console'
 import type { NextPage } from 'next'
 import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
+interface SearchCatImage {
+  id: string
+  url: string
+  width: number
+  height: number
+}
+
 const Home: NextPage = () => {
-  const fetchCatImage = async () => {
+  const [catImageUrl, setCatImageUrl] = useState("")
+
+  const fetchCatImage = async (): Promise<SearchCatImage> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search")
   const result = await res.json()// JSONに変換する
-  console.log(result)
+  // console.log(result[0])
+  return result[0]
   }
+
+  const handleClick = async () => {
+    const catImage = await fetchCatImage()
+    setCatImageUrl(catImage.url)
+  }
+
   return (
     <div className={styles.container}>
       <h1>猫画像アプリ</h1>
-        <img src="https://cdn2.thecatapi.com/images/ac4.jpg" alt=""
+        <img src={catImageUrl} alt=""
         width={500}
         height="auto"
         />
-        <button className={styles.btn} onClick={fetchCatImage}>今日の猫さん</button>
+        <button className={styles.btn} onClick={handleClick}>今日の猫さん</button>
     </div>
   )
 }
